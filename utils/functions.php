0a0b49858546d671bb68b1cf3d707d67c44d6e8d
koslinj <koslinj@gmail.com>
2023-10-29 14:14:36 +0100

    fix in functions.php to proper assoc table

--- utils/functions.php
+++ utils/functions.php
@@ -1,25 +1,21 @@
 <?php
+require_once("utils/db.php");
 function getProductByName(string $productName)
 {
-    require("utils/db.php");
+    global $conn;
     $stmt = $conn->prepare("SELECT * FROM products WHERE productName = :product");
     $stmt->bindParam(':product', $productName);
     $stmt->execute();
-    $product = $stmt->fetchAll();
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $product[0];
 }
 
 function getCategoriesByProductId(int $productId)
 {
-    require("utils/db.php");
+    global $conn;
     $stmt = $conn->prepare("SELECT c.category FROM categories AS c JOIN products_categories AS pc ON c.categoryId = pc.categoryId WHERE pc.productId = :id");
     $stmt->bindParam(':id', $productId);
     $stmt->execute();
-    $categories = $stmt->fetchAll();
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $categories;
 }
-
