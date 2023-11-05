e52e08ee291a3c62633fa21dd4232917974bea3b
koslinj <koslinj@gmail.com>
2023-11-05 22:20:46 +0100

    clear order in SESSION on logout

--- utils/logout.php
+++ utils/logout.php
@@ -6,6 +6,10 @@ if (isset($_SESSION['user_id'])) {
     unset($_SESSION['user_id']);
 }
 
+if (isset($_SESSION['order'])) {
+    unset($_SESSION['order']);
+}
+
 // Redirect to the login page
 header("Location: ../login_register.php");
 exit();
