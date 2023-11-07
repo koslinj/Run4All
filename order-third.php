a45918bdaf72dccf4240a5221bf84c2dca68ab5e
koslinj <koslinj@gmail.com>
2023-11-07 18:26:45 +0100

    clearing cart after finishing order, also change in db - remove unique

--- order-third.php
+++ order-third.php
@@ -15,9 +15,18 @@ if (isset($_SESSION['order']) && isset($_SESSION['cart'])) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["final"])) {
-    if (isset($_SESSION['user_id']))
+//    if (isset($_SESSION['user_id'])) ????
+//    NIE WIEM PO CO TEN IF
     $orderId = insertOrder($order, $sum);
     insertOrderDetails($cart, $orderId);
+
+    if (isset($_SESSION['cart'])) {
+        unset($_SESSION['cart']);
+    }
+
+    if (isset($_SESSION['user_id'])) {
+        clearCartInDb($_SESSION['user_id']);
+    }
 
     header('Location: order-fourth.php');
     exit();
