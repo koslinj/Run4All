d9712947cee946fe7cb5096cbf1bfc57252142d0
koslinj <koslinj@gmail.com>
2023-11-08 18:42:49 +0100

    charset for safety

--- utils/db.php
+++ utils/db.php
@@ -5,7 +5,7 @@ $dbname = "iab_240712";
 $username = "root";
 $password = "";
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port;charset=utf8mb4", $username, $password);
 
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
